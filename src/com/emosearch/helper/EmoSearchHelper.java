b789f47a7da812a6155e151ee1e4e50006327fa6
wishalvs <wishalvs@gmail.com>
2013-11-16 15:58:00 -0500

    added models, changed package names

---
+++ src/com/emosearch/helper/EmoSearchHelper.java
@@ -0,0 +1,13 @@
+package com.emosearch.helper;
+
+public class EmoSearchHelper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/emosearch/model/PreferencesModel.java
@@ -0,0 +1,41 @@
+package com.emosearch.model;
+
+public class PreferencesModel {
+
+	private String preferencesId;
+	private String L1Id;
+	private String level2Id;
+	private String hsRating;
+	private String teRating;
+	public String getPreferencesId() {
+		return preferencesId;
+	}
+	public void setPreferencesId(String preferencesId) {
+		this.preferencesId = preferencesId;
+	}
+	public String getL1Id() {
+		return L1Id;
+	}
+	public void setL1Id(String l1Id) {
+		L1Id = l1Id;
+	}
+	public String getLevel2Id() {
+		return level2Id;
+	}
+	public void setLevel2Id(String level2Id) {
+		this.level2Id = level2Id;
+	}
+	public String getHsRating() {
+		return hsRating;
+	}
+	public void setHsRating(String hsRating) {
+		this.hsRating = hsRating;
+	}
+	public String getTeRating() {
+		return teRating;
+	}
+	public void setTeRating(String teRating) {
+		this.teRating = teRating;
+	}
+
+}
