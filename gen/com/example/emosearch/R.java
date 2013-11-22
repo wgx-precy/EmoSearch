0d57ba7d1afa44be17d73b0613d4075734c0a212
wgx-precy <wgx.nuaa@gmail.com>
2013-11-21 20:26:38 -0500

    overwrite

--- gen/com/example/emosearch/R.java
+++ gen/com/example/emosearch/R.java
@@ -21,43 +21,55 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int emobackground=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080013;
-        public static final int appName=0x7f08000a;
-        public static final int applogin=0x7f08000b;
-        public static final int appsignup=0x7f080010;
-        public static final int confirm_password=0x7f080011;
+        public static final int action_settings=0x7f080018;
+        public static final int appName=0x7f08000d;
+        public static final int applogin=0x7f08000e;
+        public static final int appsignup=0x7f080014;
+        public static final int confirm_password=0x7f080015;
         public static final int level1_bar=0x7f080001;
         public static final int level1_cinema=0x7f080003;
         public static final int level1_food=0x7f080000;
         public static final int level1_shows=0x7f080002;
         public static final int level1_sports=0x7f080004;
         public static final int level1_traverl=0x7f080005;
-        public static final int login=0x7f08000e;
-        public static final int password=0x7f08000d;
-        public static final int set_preference=0x7f080006;
-        public static final int signup=0x7f08000f;
-        public static final int signup_button=0x7f080012;
-        public static final int submit_search=0x7f080009;
-        public static final int textView=0x7f080008;
-        public static final int touchView=0x7f080007;
-        public static final int username=0x7f08000c;
+        public static final int level2_cake=0x7f080007;
+        public static final int level2_fast_food=0x7f080006;
+        public static final int level2_street_vendor=0x7f080008;
+        public static final int listView=0x7f080013;
+        public static final int login=0x7f080011;
+        public static final int password=0x7f080010;
+        public static final int set_preference=0x7f080009;
+        public static final int signup=0x7f080012;
+        public static final int signup_button=0x7f080016;
+        public static final int submit_search=0x7f08000c;
+        public static final int textView=0x7f08000b;
+        public static final int textView1=0x7f080017;
+        public static final int touchView=0x7f08000a;
+        public static final int username=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_filter=0x7f030000;
-        public static final int activity_home=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_preference=0x7f030003;
-        public static final int activity_sign_up=0x7f030004;
+        public static final int activity_food=0x7f030001;
+        public static final int activity_home=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_preference=0x7f030004;
+        public static final int activity_result=0x7f030005;
+        public static final int activity_sign_up=0x7f030006;
+        public static final int listrow_details=0x7f030007;
+        public static final int listrow_group=0x7f030008;
     }
     public static final class menu {
         public static final int filter=0x7f070000;
-        public static final int home=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int preference=0x7f070003;
-        public static final int sign_up=0x7f070004;
+        public static final int food=0x7f070001;
+        public static final int home=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int preference=0x7f070004;
+        public static final int result=0x7f070005;
+        public static final int sign_up=0x7f070006;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -77,8 +89,10 @@ public final class R {
         public static final int signup=0x7f050005;
         public static final int submit=0x7f050003;
         public static final int title_activity_filter=0x7f050016;
+        public static final int title_activity_food=0x7f050017;
         public static final int title_activity_home=0x7f05000d;
         public static final int title_activity_preference=0x7f05000f;
+        public static final int title_activity_result=0x7f050018;
         public static final int title_activity_sign_up=0x7f05000b;
         public static final int userlogin=0x7f050009;
         public static final int username=0x7f050006;
